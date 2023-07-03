cd781af6cfff8c173c443cd29a428d50a6b1980b
Nthr0 <138446143+Nthr0@users.noreply.github.com>
2023-07-03 10:00:36 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World 0-0</h1>
+      <p>I don't get typescript at all (╯°□°）╯︵ ┻━┻</p>
+    </div>
+  );
+}
