e8b948ed74a5444adf9b3ded3297b74b5463919b
Nthr0 <carterzombieslayer@gmail.com>
2023-07-04 09:50:31 +0100

    w

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import './style.css';
 
-function MyButton(x){
-  <button>x</button>
-}
+
 export default function App() {
   return (
     <div>
       <h1>Hello World 0-0</h1>
       <p>I don't get typescript at all (╯°□°）╯︵ ┻━┻</p>
-      MyButton("Trust")
+      <button>Vroom</button>
     </div>
   );
 }
