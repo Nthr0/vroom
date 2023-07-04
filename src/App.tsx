9e60be31cbdc5f022abbc3e515ac4368d7d4dc90
Nthr0 <carterzombieslayer@gmail.com>
2023-07-04 10:30:41 +0100

    Button Css

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,28 @@ export default function App() {
     <div>
       <h1>Hello World 0-0</h1>
       <p>I don't get typescript at all (╯°□°）╯︵ ┻━┻</p>
-      <button>Vroom</button>
+      <div className="buttons">
+        <div className="row1">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <div className="row2">
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+        </div>
+        <div className="row3">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
+        <div className="row4">
+          <button>x</button>
+          <button>0</button>
+          <button>✓</button>
+        </div>
+      </div>
     </div>
   );
 }
