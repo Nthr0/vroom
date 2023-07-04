7261627c65919bb9929b64ed41de7764c2234c6a
Nthr0 <carterzombieslayer@gmail.com>
2023-07-04 09:38:43 +0100

    sicc vroom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import './style.css';
 
+function MyButton(){
+  <button>Trust</button>
+}
 export default function App() {
   return (
     <div>
       <h1>Hello World 0-0</h1>
       <p>I don't get typescript at all (╯°□°）╯︵ ┻━┻</p>
+      <MyButton/>
     </div>
   );
 }
