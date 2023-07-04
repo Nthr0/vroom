612575e8b291d09e494b70d35c101d3f81de2f50
Nthr0 <138446143+Nthr0@users.noreply.github.com>
2023-07-04 15:04:53 +0100

    1 file modified

--- src/demo-buttons.tsx
+++ src/demo-buttons.tsx
@@ -3,25 +3,30 @@ import * as React from 'react';
 export default function buttonPanel() {
   return (
     <>
-      <div className="row1">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-      </div>
-      <div className="row2">
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-      </div>
-      <div className="row3">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-      </div>
-      <div className="row4">
-        <button>x</button>
-        <button>0</button>
-        <button>✓</button>
+      <div className="bigBox">
+        <div className="row1">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+        </div>
+        <div className="row2">
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+        </div>
+        <div className="row3">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
+        <div className="row4">
+          <button>x</button>
+          <button>0</button>
+          <button>✓</button>
+        </div>
+        <div className="inputBox">
+          <p>5</p>
+        </div>
       </div>
     </>
   );
